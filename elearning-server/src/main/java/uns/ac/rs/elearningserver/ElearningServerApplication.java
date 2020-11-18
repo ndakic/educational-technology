5397ad1c1b443c471e57c403760451810329b67b
ndakic <nikoladakic@uns.ac.rs>
2020-11-18 15:54:45 +0100

    elearning-server project base

---
+++ elearning-server/src/main/java/uns/ac/rs/elearningserver/ElearningServerApplication.java
@@ -0,0 +1,13 @@
+package uns.ac.rs.elearningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElearningServerApplication.class, args);
+    }
+
+}
---
+++ elearning-server/src/test/java/uns/ac/rs/elearningserver/ElearningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uns.ac.rs.elearningserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
