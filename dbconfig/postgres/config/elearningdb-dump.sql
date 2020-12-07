f5a5c11cd5f4c3974c8e212c930ecd2d3d48600f
ndakic <nikoladakic@uns.ac.rs>
2020-12-07 09:36:10 +0100

    db update

--- dbconfig/postgres/config/elearningdb-dump.sql
+++ dbconfig/postgres/config/elearningdb-dump.sql
@@ -2,13 +2,13 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.5 (Ubuntu 12.5-0ubuntu0.20.04.1)
--- Dumped by pg_dump version 12.5 (Ubuntu 12.5-0ubuntu0.20.04.1)
+-- Dumped from database version 9.3.24
+-- Dumped by pg_dump version 13.1 (Ubuntu 13.1-1.pgdg18.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
+SET client_encoding = 'SQL_ASCII';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
@@ -20,7 +20,7 @@ SET row_security = off;
 -- Name: elearningdb; Type: DATABASE; Schema: -; Owner: postgres
 --
 
-CREATE DATABASE elearningdb WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+CREATE DATABASE elearningdb WITH TEMPLATE = template0 ENCODING = 'SQL_ASCII' LOCALE = 'C';
 
 
 ALTER DATABASE elearningdb OWNER TO postgres;
@@ -30,7 +30,7 @@ ALTER DATABASE elearningdb OWNER TO postgres;
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
+SET client_encoding = 'SQL_ASCII';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
@@ -39,8 +39,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 SET default_tablespace = '';
-
-SET default_table_access_method = heap;
 
 --
 -- Name: answer; Type: TABLE; Schema: public; Owner: euser
@@ -51,11 +49,42 @@ CREATE TABLE public.answer (
     md5h character varying(255),
     text character varying(255),
     question_id bigint NOT NULL,
-    status_id bigint NOT NULL
+    status_id bigint NOT NULL,
+    correct boolean
 );
 
 
 ALTER TABLE public.answer OWNER TO euser;
+
+--
+-- Name: answer_history; Type: TABLE; Schema: public; Owner: euser
+--
+
+CREATE TABLE public.answer_history (
+    answer_history_id bigint NOT NULL,
+    date timestamp without time zone,
+    answer_id bigint NOT NULL,
+    euser_id bigint NOT NULL,
+    question_id bigint NOT NULL,
+    test_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.answer_history OWNER TO euser;
+
+--
+-- Name: answer_history_seq; Type: SEQUENCE; Schema: public; Owner: euser
+--
+
+CREATE SEQUENCE public.answer_history_seq
+    START WITH 1
+    INCREMENT BY 50
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.answer_history_seq OWNER TO euser;
 
 --
 -- Name: answer_id_seq; Type: SEQUENCE; Schema: public; Owner: euser
@@ -211,39 +240,68 @@ ALTER TABLE public.user_id_seq OWNER TO euser;
 -- Data for Name: answer; Type: TABLE DATA; Schema: public; Owner: euser
 --
 
+INSERT INTO public.answer VALUES (1, 'dadbbc2d1715f87059f3dd6a6843655f', 'Polymorphism is the property of something having many forms.', 12, 30, NULL);
+INSERT INTO public.answer VALUES (2, '0e71d82219018ece79d090eea7e74004', 'Yes', 13, 30, NULL);
+INSERT INTO public.answer VALUES (3, 'ad635f5066226cfcc4a8f2952405e30a', 'No', 13, 30, NULL);
+INSERT INTO public.answer VALUES (5, 'ce8be9283bd853bee32eaa05e513c6bc', 'Pokemon', 12, 30, false);
+
+
+--
+-- Data for Name: answer_history; Type: TABLE DATA; Schema: public; Owner: euser
+--
+
+INSERT INTO public.answer_history VALUES (1, '2020-12-02 01:54:11.487774', 1, 1, 12, 1);
 
 
 --
 -- Data for Name: euser; Type: TABLE DATA; Schema: public; Owner: euser
 --
 
+INSERT INTO public.euser VALUES (1, 'daka@gmail.com', 'Nikola', 'Dakic', '8d4aff29071ddee43ffa150a3c7aace8', '1234', NULL, 1, 'TEACHER');
 
 
 --
 -- Data for Name: question; Type: TABLE DATA; Schema: public; Owner: euser
 --
 
+INSERT INTO public.question VALUES (12, '0eef3305e23483be88386624dd811e1a', NULL, 'What is polymorphism?', 20, 1);
+INSERT INTO public.question VALUES (13, 'b29de7be69a042654bd4ae703d0b76d8', NULL, 'Can you override a private or static method in Java?', 20, 1);
 
 
 --
 -- Data for Name: status; Type: TABLE DATA; Schema: public; Owner: euser
 --
 
-INSERT INTO public.status (status_id, description, name) VALUES (1, 'Test is Active', 'Active');
-INSERT INTO public.status (status_id, description, name) VALUES (2, 'Test is Deleted', 'Deleted');
+INSERT INTO public.status VALUES (2, 'User is Deleted', 'Deleted');
+INSERT INTO public.status VALUES (1, 'User is Active', 'Active');
+INSERT INTO public.status VALUES (10, 'Test is Active', 'Active');
+INSERT INTO public.status VALUES (11, 'Test is Deleted', 'Deleted');
+INSERT INTO public.status VALUES (20, 'Question is Active', 'Active');
+INSERT INTO public.status VALUES (21, 'Question is Deleted', 'Deleted');
+INSERT INTO public.status VALUES (30, 'Answer is Active', 'Active');
+INSERT INTO public.status VALUES (31, 'Answer is Deleted', 'Deleted');
 
 
 --
 -- Data for Name: test; Type: TABLE DATA; Schema: public; Owner: euser
 --
 
+INSERT INTO public.test VALUES (1, 'Prvi Test', 10, '71eccd018048d9e53156a94cc672afa7', NULL, NULL, NULL, 1);
+INSERT INTO public.test VALUES (4, 'Test broj 2', 10, '5ebf875dcf519e2c6292f70e3aecf922', '2020-12-02 13:01:09.652144', '2020-12-22 01:00:00', '2020-12-02 01:00:00', 1);
+
+
+--
+-- Name: answer_history_seq; Type: SEQUENCE SET; Schema: public; Owner: euser
+--
+
+SELECT pg_catalog.setval('public.answer_history_seq', 51, true);
 
 
 --
 -- Name: answer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: euser
 --
 
-SELECT pg_catalog.setval('public.answer_id_seq', 1, false);
+SELECT pg_catalog.setval('public.answer_id_seq', 6, true);
 
 
 --
@@ -257,7 +315,7 @@ SELECT pg_catalog.setval('public.euser_id_seq', 1, false);
 -- Name: question_id_seq; Type: SEQUENCE SET; Schema: public; Owner: euser
 --
 
-SELECT pg_catalog.setval('public.question_id_seq', 1, false);
+SELECT pg_catalog.setval('public.question_id_seq', 13, true);
 
 
 --
@@ -271,7 +329,7 @@ SELECT pg_catalog.setval('public.status_id_seq', 1, false);
 -- Name: test_id_seq; Type: SEQUENCE SET; Schema: public; Owner: euser
 --
 
-SELECT pg_catalog.setval('public.test_id_seq', 1, false);
+SELECT pg_catalog.setval('public.test_id_seq', 4, true);
 
 
 --
@@ -279,6 +337,14 @@ SELECT pg_catalog.setval('public.test_id_seq', 1, false);
 --
 
 SELECT pg_catalog.setval('public.user_id_seq', 1, false);
+
+
+--
+-- Name: answer_history answer_history_pkey; Type: CONSTRAINT; Schema: public; Owner: euser
+--
+
+ALTER TABLE ONLY public.answer_history
+    ADD CONSTRAINT answer_history_pkey PRIMARY KEY (answer_history_id);
 
 
 --
@@ -319,6 +385,14 @@ ALTER TABLE ONLY public.status
 
 ALTER TABLE ONLY public.test
     ADD CONSTRAINT test_pkey PRIMARY KEY (test_id);
+
+
+--
+-- Name: answer_history fk4qk2oou906c3i0gpiibc74tm8; Type: FK CONSTRAINT; Schema: public; Owner: euser
+--
+
+ALTER TABLE ONLY public.answer_history
+    ADD CONSTRAINT fk4qk2oou906c3i0gpiibc74tm8 FOREIGN KEY (answer_id) REFERENCES public.answer(answer_id);
 
 
 --
@@ -370,6 +444,22 @@ ALTER TABLE ONLY public.test
 
 
 --
+-- Name: answer_history fkjlf4iiff1df7xlgmi4ympkyq3; Type: FK CONSTRAINT; Schema: public; Owner: euser
+--
+
+ALTER TABLE ONLY public.answer_history
+    ADD CONSTRAINT fkjlf4iiff1df7xlgmi4ympkyq3 FOREIGN KEY (question_id) REFERENCES public.question(question_id);
+
+
+--
+-- Name: answer_history fkk9xw3589x0xo4rvkwa359coa8; Type: FK CONSTRAINT; Schema: public; Owner: euser
+--
+
+ALTER TABLE ONLY public.answer_history
+    ADD CONSTRAINT fkk9xw3589x0xo4rvkwa359coa8 FOREIGN KEY (euser_id) REFERENCES public.euser(euser_id);
+
+
+--
 -- Name: answer fklrc0tjc6q37hjfwqqoi5sp84q; Type: FK CONSTRAINT; Schema: public; Owner: euser
 --
 
@@ -378,10 +468,32 @@ ALTER TABLE ONLY public.answer
 
 
 --
+-- Name: answer_history fko5sj01ockkbu3yn4jaes4rcd3; Type: FK CONSTRAINT; Schema: public; Owner: euser
+--
+
+ALTER TABLE ONLY public.answer_history
+    ADD CONSTRAINT fko5sj01ockkbu3yn4jaes4rcd3 FOREIGN KEY (test_id) REFERENCES public.test(test_id);
+
+
+--
 -- Name: DATABASE elearningdb; Type: ACL; Schema: -; Owner: postgres
 --
 
+REVOKE ALL ON DATABASE elearningdb FROM PUBLIC;
+REVOKE ALL ON DATABASE elearningdb FROM postgres;
+GRANT ALL ON DATABASE elearningdb TO postgres;
+GRANT CONNECT,TEMPORARY ON DATABASE elearningdb TO PUBLIC;
 GRANT ALL ON DATABASE elearningdb TO euser;
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
