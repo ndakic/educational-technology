14bce817f9a34978b187307c4b1b39c56a8c41c9
ndakic <nikoladakic@uns.ac.rs>
2020-12-16 00:41:16 +0100

    sql update

--- elearning-server/src/main/resources/data/data.sql
+++ elearning-server/src/main/resources/data/data.sql
@@ -9,3 +9,12 @@ insert into status (status_id, name, description) values (21, 'Deleted', 'Questi
 
 insert into status (status_id, name, description) values (30, 'Active', 'Answer is Active');
 insert into status (status_id, name, description) values (31, 'Deleted', 'Answer is Deleted');
+
+insert into status (status_id, name, description) values (40, 'Active', 'Problem is Active');
+insert into status (status_id, name, description) values (41, 'Deleted', 'Problem is Deleted');
+
+insert into status (status_id, name, description) values (50, 'Active', 'Link is Active');
+insert into status (status_id, name, description) values (51, 'Deleted', 'Link is Deleted');
+
+insert into status (status_id, name, description) values (60, 'Active', 'Domain is Active');
+insert into status (status_id, name, description) values (61, 'Deleted', 'Domain is Deleted');
