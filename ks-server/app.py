20e3044622e8a813fb13b8f4096a025affdc4312
ndakic <nikoladakic@uns.ac.rs>
2020-12-23 09:44:25 +0100

    flask app for calculating knowledge space

---
+++ ks-server/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+import pandas as pd
+import numpy as np
+import sys
+
+sys.path.append('learning_spaces/')
+from learning_spaces.kst import iita
+
+
+@app.route('/knowledge-space', methods = ['POST'])
+def get_knowledge_space():
+	data = request.get_json(force=True)
+	result = calc_knowledge_space(data)
+	print(result)
+	ks = result['implications'];
+	print("Knowledge Space: ", ks)
+	return jsonify(ks)
+
+
+def calc_knowledge_space(data):
+	data_frame = pd.DataFrame(data)
+	response = iita(data_frame, v=1)
+	return response
+
+
+if __name__ == '__main__':
+    app.run()
